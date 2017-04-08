8f60f8d0e4ef170147c2b4743e00792ed79a0fdd
Harry Tri Wibowo <mail@htwibowo.co>
2017-04-08 14:40:29 +0700

    add delete button

--- ReactJS/frontend/src/components/TodoItem/index.jsx
+++ ReactJS/frontend/src/components/TodoItem/index.jsx
@@ -7,6 +7,9 @@ class TodoItem extends Component {
   render() {
     return (
         <div onClick={() => this.props.onPress()} className={"todoItem" + (this.props.selected ? ' selected' : '') }>
+            <div className='floatingAction'>
+                <button onClick={() => this.props.onDelete()}>X</button>
+            </div>
             <p>{this.props.text}</p>
         </div>
     );
